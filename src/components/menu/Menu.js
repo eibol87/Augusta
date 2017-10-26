829490caee6ba4f4a8e1b22f66255712d72bcb77
Abel <canet.ach@gmail.com>
2017-10-26 22:13:49 +0200

    componente menu

--- src/components/menu/Menu.js
+++ src/components/menu/Menu.js
@@ -5,26 +5,27 @@ import data from '../../api/menu.json'
 class Menu extends Component {
 	render(){
 		return(
-		<div className="Menu">
-	        <ul className="Menu--lu">
-				{data.map((menu,i) => {
-					return (
-						<div>
-							<li key={i} className="Menu--li--header"><span>{menu.category}</span></li>
-							{menu.items.map((items,i) =>
-								<li>
-					            	<a href="/">
-					            		<i className={`${items.icon} Menu--glyph-icon`} aria-hidden="true"></i>
-					            		<span>{items.title}</span>
-					            	</a>
-					           	</li>
-							)}
-						</div>
-					)
-				})}
-	        </ul>
-	    </div>
-	)}
+			<div className="Menu">
+		  	<ul className="Menu--lu">
+					{data.map((menu) => {
+						return (
+							<div>
+								<li key={menu.id} className="Menu--li--header"><span>{menu.category}</span></li>
+								{menu.items.map((items) =>
+									<li key={menu.id}>
+						      	<a href={items.link}>
+						      		<i className={`${items.icon} Menu--glyph-icon`} aria-hidden="true"></i>
+					         		<span>{items.title}</span>
+						       	</a>
+				         	</li>
+								)}
+							</div>
+						)
+					})}
+		    </ul>
+		  </div>
+		)
+	}
 }
 
 export default Menu
