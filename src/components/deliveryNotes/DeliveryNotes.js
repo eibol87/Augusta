c8d513ed94ca3ea02b5090e303b8d1c0df38b3e6
Abel <canet.ach@gmail.com>
2017-11-19 11:19:55 +0100

    implementado buscador por fechas

--- src/components/deliveryNotes/DeliveryNotes.js
+++ src/components/deliveryNotes/DeliveryNotes.js
@@ -34,6 +34,10 @@ class DeliveryNotes extends Component {
 				})
 			)
 	}
+	dateFormatter(cell, row) {
+		cell =new Date(cell)
+		return `${('0' + cell.getDate()).slice(-2)}/${('0' + (cell.getMonth() + 1)).slice(-2)}/${cell.getFullYear()}`;
+	}
 	
   render(){
     return(
@@ -45,7 +49,7 @@ class DeliveryNotes extends Component {
      	data={ this.state.deliveryNotes } 
      	options={{defaultSortName:'entry_date', defaultSortOrder: 'asc' }} 
      	trClassName={this.rowClassNameFormat}>
-	     <TableHeaderColumn dataField='entry_date' isKey dataSort filter={ { type: 'TextFilter', delay: 100 } }>Fecha entrada'</TableHeaderColumn>
+	     <TableHeaderColumn dataField='entry_date' dataFormat={ this.dateFormatter } isKey dataSort filter={ { type: 'DateFilter', delay: 100 } }>Fecha entrada'</TableHeaderColumn>
 	      <TableHeaderColumn dataField='customer_name' dataSort filter={ { type: 'TextFilter', delay: 100 } }>Cliente</TableHeaderColumn>
 	      <TableHeaderColumn dataField='numberOfArticles'  dataSort filter={ { type: 'TextFilter', delay: 100 } }>Número de articulos</TableHeaderColumn>
 	    </BootstrapTable>
