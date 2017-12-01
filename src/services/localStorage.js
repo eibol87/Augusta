ad2cc1ab2d9e37fbfd15bc2d4b06c5dc0ccd991e
Abel <canet.ach@gmail.com>
2017-12-01 11:19:37 +0100

    fix

--- src/services/localStorage.js
+++
@@ -1,9 +0,0 @@
-export function setSessionStorage(token){
-  sessionStorage.setItem('token', token);
-}
-export function getSessionStorage(){
-  return sessionStorage.getItem('token');
-}
-export function removeSessionStorage(){
-  return sessionStorage.removeItem('token');
-}
